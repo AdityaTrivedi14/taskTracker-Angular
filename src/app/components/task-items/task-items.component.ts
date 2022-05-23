87b8f12a071b1c233bb5f80b8a6cb06514227bce
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2022-05-24 00:30:14 +0530

    Upadate & dblclick Functionality

--- src/app/components/task-items/task-items.component.ts
+++ src/app/components/task-items/task-items.component.ts
@@ -11,6 +11,7 @@ export class TaskItemsComponent implements OnInit {
 
   @Input() task!: Task;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() { }
@@ -19,5 +20,9 @@ export class TaskItemsComponent implements OnInit {
 
   onDelete(task: Task){
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
   }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -20,4 +20,9 @@ export class TasksComponent implements OnInit {
     this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
   }
 
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe(); 
+  }
+
 }
