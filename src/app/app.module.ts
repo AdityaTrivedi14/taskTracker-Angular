e0182ceb6011fd3360d5cb5c54f32e3e16050ccd
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2022-05-24 23:48:35 +0530

    Add Task functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemsComponent } from './components/task-items/task-items.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,14 @@ import { TaskItemsComponent } from './components/task-items/task-items.component
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemsComponent
+    TaskItemsComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -25,4 +25,8 @@ export class TasksComponent implements OnInit {
     this.taskService.updateTaskReminder(task).subscribe(); 
   }
 
+  addTask(task: Task){
+    this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)));
+  }
+
 }
