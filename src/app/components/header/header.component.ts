d51836b8312698e09483165f9ebead1970938634
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2022-05-25 23:26:44 +0530

    Routes, Footer & About page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = false;
   subscription!: Subscription;
   
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
    }
 
@@ -23,4 +24,7 @@ export class HeaderComponent implements OnInit {
     this.uiService.toggleAddTask();
   }
 
+  hasRoute(route: string){
+    return this.router.url === route;
+  }
 }
